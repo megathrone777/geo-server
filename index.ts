753bacbd235bf1d4d0c75ae5cbca310193306c35
Andrii Taranenko <megathrone3333@gmail.com>
2024-11-02 02:25:39 +0100

    Some

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const PORT = 1338;
+const app = express();
+
+app.use(cors());
+app.use(express.static("public"));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("sendLocation", (data) => {
+    console.log("Received location:", data);
+    io.emit("changeLocation", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
